9b69de42ce16449733047ba35e464ae7239b7786
Gaurav kanojiya <77156667+nlmgaurav@users.noreply.github.com>
2024-08-01 06:47:03 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <HeroSection/>
+      
+    </div>
+  )
+}
+
+export default App
